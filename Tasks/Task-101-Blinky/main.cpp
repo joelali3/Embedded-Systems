4c93dc9590786168f964bc3c60b83d2cc582a768
joelali3 <joel.ali@students.plymouth.ac.uk>
2022-10-20 12:09:38 +0100

    task 101 finished
    now blinks green red and blue

--- Tasks/Task-101-Blinky/main.cpp
+++ Tasks/Task-101-Blinky/main.cpp
@@ -5,8 +5,8 @@ DigitalOut greenLED(LED1);
 
 //These are "commented out" and so are not part of the program. You can uncomment them by removing the // characters
 //Your task is to make a sequence alternating between Green+Red and just Blue 
-//DigitalOut blueLED(LED2);
-//DigitalOut redLED(LED3);
+DigitalOut blueLED(LED2);
+DigitalOut redLED(LED3);
 
 //The main function - all executable C / C++ applications have a main function. This is our entry point in the software
 int main() 
@@ -18,13 +18,15 @@ int main()
 
         // Turn onboard LED ON  
         greenLED = 1; 
-
+        redLED = 1;
+        blueLED = 0;
         // Wait 0.5 second (1 million microseconds)
         wait_us(200000); 
 
         // Turn LED OFF
         greenLED = 0;
-
+        redLED = 0;
+        blueLED = 1;
         // Wait 0.5 second
         wait_us(200000); 
     }
